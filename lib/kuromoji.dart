6d494f121a29a036776e01ba2575b5e4360391ba
Lexedia <contact@lexedia.moe>
2025-09-01 21:58:35 +0200

    Revert "yaay embeddeds dicts are gone"
    This reverts commit 69a4afecac49f4a31545875b6bfc04b4a74cec66.

--- lib/kuromoji.dart
+++ lib/kuromoji.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+import 'package:kuromoji/src/dict/data/char.dart';
 import 'src/dictionary_loader.dart';
 import 'src/dict/dynamic_dictionaries.dart';
 import 'src/tokenizer.dart';
@@ -8,8 +10,12 @@ class TokenizerBuilder {
   Future<Tokenizer> build() async {
     final loader = DictionaryLoader();
     final data = await loader.load();
-    final charDef = await loader.loadCharDef();
+    final charDef = await _loadCharDef();
     final dictionaries = DynamicDictionaries(data, charDef);
     return Tokenizer(dictionaries);
+  }
+
+  Future<Uint8List> _loadCharDef() async {
+    return charData;
   }
 }
