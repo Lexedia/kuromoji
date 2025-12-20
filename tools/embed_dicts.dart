bf39f46f2a39f56cfcb54ba8d7c39d28381d599b
Bruno D'Luka <brunodlukaa@gmail.com>
2025-12-20 11:22:51 -0300

    chore: Address feedback

--- tools/embed_dicts.dart
+++ tools/embed_dicts.dart
@@ -1,29 +1,27 @@
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:path/path.dart' as path;
 
 void main(List<String> args) async {
   final dictDir = Directory('dict');
-  if (!await dictDir.exists()) {
+  if (!dictDir.existsSync()) {
     print('Error: "dict" directory not found.');
     return;
   }
 
-  final files = await dictDir.list().where((e) => e is File).cast<File>().toList();
+  final files = dictDir.listSync().whereType<File>().toList();
 
   for (final f in files) {
     final fileName = path.basenameWithoutExtension(f.path);
     print('Processing $fileName...');
 
     final contents = await f.readAsBytes();
-    final variableName = fileName.replaceAll('.dat', '').replaceAll('.def', '') + 'Data';
+    final variableName = '${fileName.replaceAll('.dat', '').replaceAll('.def', '')}Data';
 
-    // Use base64 encoding to support all file lengths safely
     final encoded = base64Encode(contents);
 
-    await File(
-      'lib/src/dict/data/$fileName.dart',
-    ).writeAsString('''
+    await File('lib/src/dict/data/$fileName.dart').writeAsString('''
 import 'dart:convert';
 import 'dart:typed_data';
 
